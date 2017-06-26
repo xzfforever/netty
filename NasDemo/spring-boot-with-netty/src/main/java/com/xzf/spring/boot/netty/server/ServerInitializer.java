981eddaf44e5c1c3e3dc7f58ba94a13c8df94830
xzf <270035679@qq.com>
2017-06-26 09:09:43 +0800

    be better

--- NasDemo/spring-boot-with-netty/src/main/java/com/xzf/spring/boot/netty/server/ServerInitializer.java
+++ NasDemo/spring-boot-with-netty/src/main/java/com/xzf/spring/boot/netty/server/ServerInitializer.java
@@ -17,7 +17,7 @@ public class ServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         socketChannel.pipeline().addLast(new HttpServerCodec());
-        socketChannel.pipeline().addLast(new HttpObjectAggregator(1024*1024));
+        socketChannel.pipeline().addLast(new HttpObjectAggregator(10*1024*1024));
         socketChannel.pipeline().addLast("http-handler",httpRequestHandler);
     }
 }
